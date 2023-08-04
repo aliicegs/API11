2ca42f7afc81239bef055f1848166a712e140b1c
aliicegs <132668655+aliicegs@users.noreply.github.com>
2023-08-04 11:19:28 +0000

    importando

--- index.js
+++ index.js
@@ -12,3 +12,19 @@ app.get("/", (req, res) => {
 app.listen(port, () => {            
   console.log(`Serviço escutando na porta:  ${port}`);
 });
+
+import dotenv from "dotenv";
+
+dotenv.config();
+
+import { selectUsuarios } from "./bd.js";
+
+app.get("/usuarios", async (req, res) => {
+  console.log("Rota GET/usuarios solicitada");
+  try {
+    const usuarios = await selectUsuarios();
+    res.json(usuarios);
+  } catch (error) {
+    res.status(error.status || 500).json({ message: error.message || "Erro!" });
+  }
+});
