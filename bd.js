b78e9546b3f03e6a3fe1c34abc2acc4f1472fb1d
aliicegs <132668655+aliicegs@users.noreply.github.com>
2023-08-11 11:08:11 +0000

    atualizando

--- bd.js
+++ bd.js
@@ -14,4 +14,12 @@ async function selectUsuarios() {
   return res.rows;
 }
 
-export { selectUsuarios };
+export { selectUsuarios, selectUsuario };
+
+async function selectUsuario(id) {
+  const client = await connect();
+  const query = "SELECT * FROM usuario WHERE id = $1";
+  const usuario = [id];
+  const res = await client.query(query, usuario);
+  return res.rows;
+}
