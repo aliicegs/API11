9c257327bc5768ffa0b18ec35227ba75f50c0e8c
aliicegs <132668655+aliicegs@users.noreply.github.com>
2023-08-31 13:18:22 +0000

    atividade 14

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import express from "express";      
+const app = express();             
+const port = 3000;                
+
+import dotenv from "dotenv";
+dotenv.config();
+
+import roteadorUsuario from "./routes/usuario.js";
+
+app.use(roteadorUsuario);
+
+app.use(express.json());
+
+app.get("/", (req, res) => {        
+  res.json({
+    nome: "Alice Gonçalves Silva",     
+  });
+  console.log("Rota / solicitada");
+});
+
+app.listen(port, () => {            
+  console.log(`Serviço escutando na porta:  ${port}`);
+});
